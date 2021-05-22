2c3faf28f88476c7d9967d9d62febf7da1c821c5
Christopher Ray <vde435@gmail.com>
2021-05-22 06:26:03 -0500

    Create atag2.rb

---
+++ atag2.rb
@@ -0,0 +1,11 @@
+print "This is the atag program\n"
+textin = File.open("links.txt", "r")
+textout = File.open("ataglinks.txt", "w")
+while !textin.eof?
+    line = textin.readline
+    line2 = line.chop
+    puts line2 + "\n"
+    puts "Name: "
+    name = gets.chomp
+    textout.write "<a href=#{line2}>#{name}</a><br>\n"
+end
