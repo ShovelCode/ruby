6e0bf4f557c8ca15eaeebbb498099a00e120e185
Christopher Ray <vde435@gmail.com>
2021-05-24 20:08:39 -0500

    Create remove-new-lines.rb

---
+++ remove-new-lines.rb
@@ -0,0 +1,13 @@
+#remaking the remove lines program
+ 
+puts "This is the remove new lines program"
+puts Dir.pwd
+textin = File.open("manylines.txt.txt", "r")
+textout = File.open("oneline.txt.txt", "w")
+line = "";
+while !textin.eof?
+        line << textin.readline << " "
+     end
+line.delete!("\n") 
+#line.delete!("\r")
+textout.write(line)
